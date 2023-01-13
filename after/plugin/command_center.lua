5e6dc522b8c920830d990338074afb4ca0f53373
ziwift <ziou.fang@swift-nav.com>
2023-01-13 17:37:02 +1100

    func to close all hidden buffer

--- after/plugin/command_center.lua
+++ after/plugin/command_center.lua
@@ -46,4 +46,30 @@ command_center.add({
 		desc = "Glow (Markdown Preview)",
 		cmd = "<Cmd>Glow<Cr>",
 	},
+	{
+		desc = "BufferCloseHidden",
+		cmd = "<Cmd>BufferCloseHidden<Cr>",
+	},
 }, command_center.mode.ADD)
+
+vim.cmd([[
+command! -nargs=* BufferCloseHidden call BufferCloseHidden()
+function! BufferCloseHidden()
+  " figure out which buffers are visible in any tab
+  let visible = {}
+  for t in range(1, tabpagenr('$'))
+    for b in tabpagebuflist(t)
+      let visible[b] = 1
+    endfor
+  endfor
+  " close any buffer that are loaded and not visible
+  let l:tally = 0
+  for b in range(1, bufnr('$'))
+    if bufloaded(b) && !has_key(visible, b)
+      let l:tally += 1
+      exe 'bdelete! ' . b
+    endif
+  endfor
+  echon "Deleted " . l:tally . " buffers"
+endfun
+]])
