9d8fc30f02ccd5635840c089fbb3f9fdbbc13085
ziwift <ziou.fang@swift-nav.com>
2023-01-13 23:26:14 +1100

    keymap.set helfer func

--- lua/zi/keymaps.lua
+++ lua/zi/keymaps.lua
@@ -1,62 +1,69 @@
+-- normal mapping
+local function nmap(keys, cmd, opts)
+	local opts = opts or {}
+	vim.keymap.set("n", keys, cmd, opts)
+end
+
 vim.g.mapleader = " "
 
 -- open lazygit on cwd in a newtab
-vim.keymap.set("n", "<leader><leader>", "<Cmd>tabnew | term lazygit<Cr>i")
+nmap("<leader><leader>", "<Cmd>tabnew | term lazygit<Cr>i")
 
 -- telescope
 local telescope = require("telescope.builtin")
 local ivy = require("telescope.themes").get_ivy({})
 local zilescope = require("zi/zilescope")
 
-vim.keymap.set("n", "<leader>ff", telescope.find_files, {})
-vim.keymap.set("n", "<leader>fd", telescope.buffers, {})
-vim.keymap.set("n", "<leader>fr", telescope.oldfiles, {})
-vim.keymap.set("n", "<leader>fc", telescope.command_history, {})
-vim.keymap.set("n", "<leader>ft", telescope.resume, {})
-vim.keymap.set("n", "<leader>fy", "<Cmd>Telescope neoclip<Cr>", {})
-vim.keymap.set("n", "<leader>fh", telescope.help_tags, {})
+nmap("<leader>ff", telescope.find_files, {})
+nmap("<leader>fd", telescope.buffers, {})
+nmap("<leader>fr", telescope.oldfiles, {})
+nmap("<leader>fc", telescope.command_history, {})
+nmap("<leader>ft", telescope.resume, {})
+nmap("<leader>fy", "<Cmd>Telescope neoclip<Cr>", {})
+nmap("<leader>fh", telescope.help_tags, {})
 -- some keymaps are defined in lsp.lua, to utilize on_attach
-vim.keymap.set("n", "<leader>fe", function()
+nmap("<leader>fe", function()
 	telescope.diagnostics({ bufnr = 0 })
 end, {})
-vim.keymap.set("n", "<leader>fE", telescope.diagnostics, {})
+nmap("<leader>fE", telescope.diagnostics, {})
 
-vim.keymap.set("n", "<leader>p", function()
+nmap("<leader>p", function()
 	zilescope.fd_projects(ivy)
 end, {})
 vim.api.nvim_create_user_command("ZiFdProjects", function()
 	zilescope.fd_projects(ivy)
 end, {})
 
-vim.keymap.set("n", "<leader>r", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
-vim.keymap.set("n", "<leader>b", telescope.current_buffer_fuzzy_find, {})
+nmap("<leader>r", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
+nmap("<leader>R", telescope.live_grep)
+nmap("<leader>b", telescope.current_buffer_fuzzy_find, {})
 
 -- lsp
 -- See `:h vim.lsp.*`
 local bufopts = { noremap = true, silent = true }
-vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, bufopts)
-vim.keymap.set("n", "<leader>ls", vim.lsp.buf.signature_help, bufopts)
-vim.keymap.set("n", "<leader>lR", vim.lsp.buf.rename, bufopts)
-vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, bufopts)
-vim.keymap.set("n", "<leader>le", ":LspRestart<Cr>:sleep 1<Cr>:e<Cr>", bufopts) -- TOOD: might be a better way
-vim.keymap.set("n", "<leader>ll", vim.diagnostic.open_float, {})
-vim.keymap.set("n", "<leader>ln", vim.diagnostic.goto_next, {})
-vim.keymap.set("n", "<leader>lp", vim.diagnostic.goto_prev, {})
-vim.keymap.set("n", "<leader>lr", telescope.lsp_references, {})
+nmap("gd", vim.lsp.buf.definition, bufopts)
+nmap("gr", vim.lsp.buf.references, bufopts)
+nmap("K", vim.lsp.buf.hover, bufopts)
+nmap("<leader>la", vim.lsp.buf.code_action, bufopts)
+nmap("<leader>ls", vim.lsp.buf.signature_help, bufopts)
+nmap("<leader>lR", vim.lsp.buf.rename, bufopts)
+nmap("<leader>lf", vim.lsp.buf.format, bufopts)
+nmap("<leader>le", ":LspRestart<Cr>:sleep 1<Cr>:e<Cr>", bufopts) -- TOOD: might be a better way
+nmap("<leader>ll", vim.diagnostic.open_float, {})
+nmap("<leader>ln", vim.diagnostic.goto_next, {})
+nmap("<leader>lp", vim.diagnostic.goto_prev, {})
+nmap("<leader>lr", telescope.lsp_references, {})
 -- rust tools specific defined in rust_tools.lua
-vim.keymap.set("n", "<leader>lh", "<Cmd>RustSetInlayHints<Cr>", {})
-vim.keymap.set("n", "<leader>lH", "<Cmd>RustUnsetInlayHints<Cr>", {})
+nmap("<leader>lh", "<Cmd>RustSetInlayHints<Cr>", {})
+nmap("<leader>lH", "<Cmd>RustUnsetInlayHints<Cr>", {})
 
 -- git, more keymaps defined in gitsigns
-vim.keymap.set("n", "<leader>gc", telescope.git_branches, {})
+nmap("<leader>gc", telescope.git_branches, {})
 
 -- folding
-vim.keymap.set("n", "<leader>z", "za", {})
-vim.keymap.set("n", "<leader>vf", "<Cmd>loadview<Cr>", {})
-vim.keymap.set("n", "<leader>vF", "<Cmd>mkview<Cr>", {})
+nmap("<leader>z", "za", {})
+nmap("<leader>vf", "<Cmd>loadview<Cr>", {})
+nmap("<leader>vF", "<Cmd>mkview<Cr>", {})
 
 -- custom commands
 vim.cmd([[:command! Ve e ~/.config/nvim/init.lua]])
@@ -65,11 +72,11 @@ vim.cmd([[:command! Vs so ~/.config/nvim/init.lua]])
 -- vim.keymap.set("n", "<C-c>", ":close<Cr>", { silent = true }) -- close will not close the last window
 
 -- hit '/' highlights then enter search mode
-vim.keymap.set("n", "/", ":set hls<Cr>/")
+nmap("/", ":set hls<Cr>/")
 
 -- vim.keymap.set("n", "<leader>s", ":.,$s///gc<Left><Left><Left><Left>")
-vim.keymap.set("n", "<leader>s", ":set hls<Cr>:.,$S///gc" .. string.rep("<Left>", 4)) -- Subvert from vim-abolish
-vim.keymap.set("n", "<leader>S", ":set hls<Cr>:%S///gc" .. string.rep("<Left>", 4)) -- Subvert from vim-abolish
+nmap("<leader>s", ":set hls<Cr>:.,$S///gc" .. string.rep("<Left>", 4)) -- Subvert from vim-abolish
+nmap("<leader>S", ":set hls<Cr>:%S///gc" .. string.rep("<Left>", 4)) -- Subvert from vim-abolish
 
 -- vim.keymap.set("n", "<C-J>", "<C-W><C-J>")
 -- vim.keymap.set("n", "<C-K>", "<C-W><C-K>")
@@ -81,33 +88,43 @@ vim.keymap.set({ "n", "t" }, "<C-l>", "<CMD>NavigatorRight<CR>")
 vim.keymap.set({ "n", "t" }, "<C-k>", "<CMD>NavigatorUp<CR>")
 vim.keymap.set({ "n", "t" }, "<C-j>", "<CMD>NavigatorDown<CR>")
 
-vim.keymap.set("n", "<leader>w", "<C-W>=")
+nmap("<leader>w", "<C-W>=")
 
-vim.keymap.set("n", "\\", ":vert sb<Cr><C-w>=")
+nmap("\\", ":vert sb<Cr><C-w>=")
 --
-vim.keymap.set("n", "Q", "@q")
-vim.keymap.set("n", "H", "^")
-vim.keymap.set("n", "L", "$")
+nmap("Q", "@q")
+nmap("H", "^")
+nmap("L", "$")
 
 -- toggle highlight
 vim.keymap.set("n", "<leader>h", "<Cmd>set hls!<Cr>")
+-- toggle cmdheight, good for recording macro
+local function toggle_cmdline()
+	vim.pretty_print(vim.opt.cmdheight)
+	if vim.opt.cmdheight:get() > 0 then
+		vim.opt.cmdheight = 0
+	else
+		vim.opt.cmdheight = 1
+	end
+end
+nmap("<leader>vc", toggle_cmdline)
 
-vim.keymap.set("n", "<leader>cd", "<Cmd>cd %:p:h<Cr>")
+nmap("<leader>cd", "<Cmd>cd %:p:h<Cr>")
 -- copy correct dir to clipboard
-vim.keymap.set("n", "<leader>cp", '<Cmd>let @+ = expand("%:p:h")<Cr>')
+nmap("<leader>cp", '<Cmd>let @+ = expand("%:p:h")<Cr>')
 -- chmod a+x on current buffer
-vim.keymap.set("n", "<leader>cx", "<Cmd>:!chmod a+x %<Cr>")
+nmap("<leader>cx", "<Cmd>:!chmod a+x %<Cr>")
 
 -- replacing gx, open url in browser, since netrw is generally disabled
-vim.keymap.set("n", "gx", [[:execute '!open ' . shellescape(expand('<cfile>'), 1)<CR>]])
+nmap("gx", [[:execute '!open ' . shellescape(expand('<cfile>'), 1)<CR>]])
 
-vim.keymap.set("n", "Y", '"+y$')
+nmap("Y", '"+y$')
 
 -- " c and x to blackhole
-vim.keymap.set("n", "c", '"_c')
-vim.keymap.set("n", "C", '"_C')
-vim.keymap.set("n", "x", '"_x')
-vim.keymap.set("n", "X", '"_X')
+nmap("c", '"_c')
+nmap("C", '"_C')
+nmap("x", '"_x')
+nmap("X", '"_X')
 
 vim.keymap.set("v", ">", "^o^><Esc>gv")
 vim.keymap.set("v", "<", "0o0<<Esc>gv")
@@ -131,19 +148,19 @@ function! ToggleVertQuickfix()
     endif
 endfunction
 ]])
-vim.keymap.set("n", "<leader>qq", "<Cmd>call ToggleQuickfix()<Cr>")
-vim.keymap.set("n", "<leader>qv", "<Cmd>call ToggleVertQuickfix()<Cr>")
-vim.keymap.set("n", "<leader>qj", "<Cmd>silent! cnewer<Cr>")
-vim.keymap.set("n", "<leader>qk", "<Cmd>silent! colder<Cr>")
-vim.keymap.set("n", "<leader>qi", require("replacer").run, { silent = true })
+nmap("<leader>qq", "<Cmd>call ToggleQuickfix()<Cr>")
+nmap("<leader>qv", "<Cmd>call ToggleVertQuickfix()<Cr>")
+nmap("<leader>qj", "<Cmd>silent! cnewer<Cr>")
+nmap("<leader>qk", "<Cmd>silent! colder<Cr>")
+nmap("<leader>qi", require("replacer").run, { silent = true })
 
 -- closing all buffers referenced in the qf list (after doing search replace etc.)
 -- cdo bd
 
 -- alt key mapping
-vim.keymap.set("n", "<A-s>", "<Cmd>silent! update<Cr>") -- update doesnt overwrite if file is unchanged like :w would do
+nmap("<A-s>", "<Cmd>silent! update<Cr>") -- update doesnt overwrite if file is unchanged like :w would do
 vim.keymap.set("i", "<A-s>", "<Esc>:silent! update<Cr>")
-vim.keymap.set("n", "<A-x>", "<Cmd>Telescope command_center<Cr>")
+nmap("<A-x>", "<Cmd>Telescope command_center<Cr>")
 
 -- Cmd Copy Paste for MacOs in neovide or other GUI
 vim.keymap.set("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
