0171ef8a2c9d516e59525c859e3c2f8a9ce214e6
zioufang <zioufang@gmail.com>
2023-01-15 16:36:14 +1100

    fix split for zilescope project

--- lua/zi/zilescope.lua
+++ lua/zi/zilescope.lua
@@ -2,37 +2,10 @@ local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
 local sorters = require("telescope.sorters")
 local actions = require("telescope.actions")
-local actions_state = require("telescope.actions.state")
-local entry_display = require("telescope.pickers.entry_display")
+-- local actions_state = require("telescope.actions.state")
+-- local entry_display = require("telescope.pickers.entry_display")
 
 M = {}
-
--- action can be `edit`, `vsplit`, `split`
-local function enter_dir(prompt_bufnr, act)
-	local selected = actions_state.get_selected_entry()
-	if selected == nil then
-		actions.close(prompt_bufnr)
-		return
-	end
-	local dir = selected.value
-	-- TODO: the very first dir entered doesn't change cwd for some reason
-	vim.api.nvim_set_current_dir(dir)
-
-	local cmd = "silent " .. act .. " " .. dir
-	vim.cmd(cmd)
-end
-
-local function enter_dir_edit(prompt_bufnr)
-	enter_dir(prompt_bufnr, "edit!")
-end
-
-local function enter_dir_vsplit(prompt_bufnr)
-	enter_dir(prompt_bufnr, "vsplit")
-end
-
-local function enter_dir_split(prompt_bufnr)
-	enter_dir(prompt_bufnr, "split")
-end
 
 -- TODO: introduce recency, frequency etc.
 function M.fd_projects(theme)
@@ -80,9 +53,9 @@ function M.fd_projects(theme)
 			finder = finders.new_oneshot_job(cmd, job_opts),
 			sorter = sorters.get_generic_fuzzy_sorter(),
 			attach_mappings = function(_, map)
-				map("i", "<Cr>", enter_dir_edit)
-				map("i", "<C-v>", enter_dir_vsplit)
-				map("i", "<C-s>", enter_dir_split)
+				map("i", "<Cr>", actions.file_edit)
+				map("i", "<C-v>", actions.file_vsplit)
+				map("i", "<C-s>", actions.file_split)
 				return true
 			end,
 		})
