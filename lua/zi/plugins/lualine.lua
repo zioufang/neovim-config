66537cad26c04f84d8ad2c11d5df07779f85e2c7
zioufang <zioufang@gmail.com>
2023-01-16 14:28:44 +1100

    better HOME replacement func + prettier winbar for oil buffer

--- lua/zi/plugins/lualine.lua
+++ lua/zi/plugins/lualine.lua
@@ -1,20 +1,22 @@
+-- maximized status for maximize.nvim
 local function maximize_status()
-	return vim.t.maximized and " " or ""
+  return vim.t.maximized and " " or ""
 end
 
+-- show name of the lsp server for current buf
 local function lsp_provider()
-	local clients = {}
-	for _, client in pairs(vim.lsp.buf_get_clients()) do
-		if client.name == "pyright" then
-			-- add (venv) if customized python path is detected
-			if client.config.settings.python["pythonPath"] ~= nil then
-				clients[#clients + 1] = client.name .. "(venv)"
-			end
-		else
-			clients[#clients + 1] = client.name
-		end
-	end
-	return table.concat(clients, " ")
+  local clients = {}
+  for _, client in pairs(vim.lsp.buf_get_clients()) do
+    if client.name == "pyright" then
+      -- add (venv) if customized python path is detected
+      if client.config.settings.python["pythonPath"] ~= nil then
+        clients[#clients + 1] = client.name .. "(venv)"
+      end
+    else
+      clients[#clients + 1] = client.name
+    end
+  end
+  return table.concat(clients, " ")
 end
 
 -- -- check if current branch of cwd is behind and ahead in commits compared to remote upstream
@@ -52,15 +54,28 @@ end
 -- end
 -- _G.Gstatus_timer:start(2 * 000, 0, vim.schedule_wrap(update_gstatus))
 
--- assuming two parent dirs before cwd,
--- /Usrs/username/(cwd)
+-- show cwd with icon and HOME replaced with ~
 local function current_working_dir()
-	local path = vim.split(vim.fn.getcwd(), "/")
-	local cwd = { unpack(path, 4, #path) }
-	local display = table.concat(cwd, "/")
-	return "ﮟ ~/" .. display
+  local home = os.getenv("HOME") or ""
+  local cwd = vim.fn.getcwd()
+  cwd = cwd:gsub(home, "~")
+  return "ﮟ " .. cwd
 end
 
+-- extension for oil.nvim
+local function oil_path()
+  local home = os.getenv("HOME") or ""
+  local buf_name = vim.fn.expand("%")
+  buf_name = buf_name:gsub("oil://", "")
+  buf_name = buf_name:gsub(home, "~")
+  return " "..buf_name
+end
+
+local oil = { winbar = {
+  lualine_c = { oil_path },
+}, filetypes = { "oil" } }
+
+-- custome color theme
 local custom_auto = require("lualine.themes.auto")
 local b_white = "#cccccc"
 local c_white = "#aaaaaa"
@@ -78,85 +93,86 @@ custom_auto.terminal["c"]["fg"] = c_white
 custom_auto.replace["c"]["fg"] = c_white
 
 require("lualine").setup({
-	options = {
-		theme = custom_auto,
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
-		disabled_filetypes = {
-			statusline = {},
-			winbar = { "packer", "NvimTree", "aerial", "qf", "" }, -- "" filetype is terminal
-		},
-		globalstatus = true,
-	},
-	sections = {
-		lualine_a = { "mode" },
-		lualine_b = {
-			{
-				current_working_dir,
-			},
-		},
-		lualine_c = {
-			{
-				"branch",
-				padding = { left = 1, right = 0 },
-			},
-			{ "diff", separator = "|" },
-		},
-		lualine_x = {
-			{ maximize_status, separator = "|" },
-			{
-				"aerial",
-				dense = true,
-				separator = "|",
-				color = { fg = "#f28534" },
-			},
-			{ "diagnostics", separator = "|" },
-			{
-				"filetype",
-				icon_only = true,
-				padding = { left = 1, right = 1 },
-			},
-			{ lsp_provider },
-		},
-		lualine_y = {},
-		lualine_z = { "os.date('%H:%M')" }, -- strftime() format string
-	},
-	-- TODO make oil buffer pretty in winbar
-	winbar = {
-		lualine_c = {
-			{
-				"filetype",
-				icon_only = true,
-				padding = { left = 1, right = 0 },
-			},
-			{
-				"filename",
-				path = 1,
-				color = { fg = "White" },
-				symbols = {
-					modified = "[+]",
-					readonly = "",
-					unnamed = "[Home]",
-				},
-			},
-		},
-	},
-	inactive_winbar = {
-		lualine_c = {
-			{
-				"filetype",
-				icon_only = true,
-				padding = { left = 1, right = 0 },
-			},
-			{
-				"filename",
-				path = 1,
-				symbols = {
-					modified = "[+]",
-					readonly = "",
-					unnamed = "[Home]",
-				},
-			},
-		},
-	},
+  options = {
+    theme = custom_auto,
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
+    disabled_filetypes = {
+      statusline = {},
+      winbar = { "packer", "NvimTree", "aerial", "qf", "" }, -- vanilla terminal has "" as filetype
+    },
+    globalstatus = true,
+  },
+  sections = {
+    lualine_a = { "mode" },
+    lualine_b = {
+      {
+        current_working_dir,
+      },
+    },
+    lualine_c = {
+      {
+        "branch",
+        padding = { left = 1, right = 0 },
+      },
+      { "diff", separator = "|" },
+    },
+    lualine_x = {
+      { maximize_status, separator = "|" },
+      {
+        "aerial",
+        dense = true,
+        separator = "|",
+        color = { fg = "#f28534" },
+      },
+      { "diagnostics", separator = "|" },
+      {
+        "filetype",
+        icon_only = true,
+        padding = { left = 1, right = 1 },
+      },
+      { lsp_provider },
+    },
+    lualine_y = {},
+    lualine_z = { "os.date('%H:%M')" }, -- strftime() format string
+  },
+  -- TODO make oil buffer pretty in winbar
+  winbar = {
+    lualine_c = {
+      {
+        "filetype",
+        icon_only = true,
+        padding = { left = 1, right = 0 },
+      },
+      {
+        "filename",
+        path = 1,
+        color = { fg = "White" },
+        symbols = {
+          modified = "[+]",
+          readonly = "",
+          unnamed = "[Home]",
+        },
+      },
+    },
+  },
+  inactive_winbar = {
+    lualine_c = {
+      {
+        "filetype",
+        icon_only = true,
+        padding = { left = 1, right = 0 },
+      },
+      {
+        "filename",
+        path = 1,
+        symbols = {
+          modified = "[+]",
+          readonly = "",
+          unnamed = "[Home]",
+        },
+      },
+    },
+  },
+  extensions = { oil },
 })
