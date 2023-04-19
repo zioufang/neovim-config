feb7320ef3020c935ec8caf130b7470d3036ecdf
zioufang <zioufang@gmail.com>
2023-04-19 12:03:38 +1000

    close visible terminal

--- lua/zi/terminal.lua
+++ lua/zi/terminal.lua
@@ -2,10 +2,41 @@ local fn = vim.fn
 local api = vim.api
 local cmd = vim.cmd
 local fmt = string.format
+
+local function get_visible_terminal_buffer()
+	-- Get all the windows in the current tabpage
+	local win_ids = vim.api.nvim_tabpage_list_wins(0)
+	print(vim.inspect(win_ids))
+
+	for _, win_id in ipairs(win_ids) do
+		-- Get the buffer associated with the window
+		local bufnr = vim.api.nvim_win_get_buf(win_id)
+
+		-- Get the buffer's filetype
+		local buf_ft = vim.api.nvim_buf_get_option(bufnr, "buftype")
+		print(buf_ft)
+
+		-- Check if the filetype is 'terminal'
+		if buf_ft == "terminal" then
+			return win_id
+		end
+	end
+
+	return 0
+end
+
 -- toggle term with a specific buf name
+-- assuming max of one visible term buffer per tab
+-- TODO: remember cursor position
 local function toggle_term(buf_name)
-	local window_id = fn.win_getid(fn.bufwinnr(buf_name))
+	-- if any visible terminal exists
+	local window_id = get_visible_terminal_buffer()
+
+	-- only check terminal buffer with a specific name
+	-- local window_id = fn.win_getid(fn.bufwinnr(buf_name))
+
 	local existed = fn.bufexists(buf_name)
+	print(window_id)
 
 	if window_id > 0 then
 		api.nvim_win_hide(window_id)
