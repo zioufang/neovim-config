2f3c6923f7a91a0247412ffb9c2835ba814372bc
zioufang <zioufang@gmail.com>
2024-02-05 13:35:17 +1100

    commentary: set hcl comment string manually

--- lua/zi/plugins/editing.lua
+++ lua/zi/plugins/editing.lua
@@ -1,47 +1,53 @@
 return {
-	-- gc for comment
-	{
-		"numToStr/Comment.nvim",
-		event = "VeryLazy",
-		config = true,
-	},
+  -- gc for comment
+  {
+    "numToStr/Comment.nvim",
+    event = "VeryLazy",
+    config = function()
+      local ft = require('Comment.ft')
+      ft.set('hcl', '# %s')
+      require("Comment").setup({
+        ignore = '^$', -- ignore empty lines
+      })
+    end,
+  },
 
-	{
-		"windwp/nvim-autopairs",
-		event = "VeryLazy",
-		config = true,
-	},
+  {
+    "windwp/nvim-autopairs",
+    event = "VeryLazy",
+    config = true,
+  },
 
-	{
-		"kylechui/nvim-surround",
-		event = "VeryLazy",
-		config = true,
-	},
+  {
+    "kylechui/nvim-surround",
+    event = "VeryLazy",
+    config = true,
+  },
 
-	-- better substitution + case coercion
-	{ "tpope/vim-abolish", event = "VeryLazy" },
-	-- preview support for abolish
-	{
-		"markonm/traces.vim",
-		event = "VeryLazy",
-		config = function()
-			vim.g.traces_abolish_integration = 1
-		end,
-	},
+  -- better substitution + case coercion
+  { "tpope/vim-abolish", event = "VeryLazy" },
+  -- preview support for abolish
+  {
+    "markonm/traces.vim",
+    event = "VeryLazy",
+    config = function()
+      vim.g.traces_abolish_integration = 1
+    end,
+  },
 
-	-- doc generation
-	{
-		"danymat/neogen",
-		event = "VeryLazy",
-		keys = { { "<leader>d", "<Cmd>lua require('neogen').generate()<Cr>" } },
-		config = true,
-		dependencies = "nvim-treesitter/nvim-treesitter",
-	},
+  -- doc generation
+  {
+    "danymat/neogen",
+    event = "VeryLazy",
+    keys = { { "<leader>d", "<Cmd>lua require('neogen').generate()<Cr>" } },
+    config = true,
+    dependencies = "nvim-treesitter/nvim-treesitter",
+  },
 
-	-- iv + av for subwords in snake/camel cases
-	{
-		"kana/vim-textobj-user",
-		event = "VeryLazy",
-		dependencies = "Julian/vim-textobj-variable-segment",
-	},
+  -- iv + av for subwords in snake/camel cases
+  {
+    "kana/vim-textobj-user",
+    event = "VeryLazy",
+    dependencies = "Julian/vim-textobj-variable-segment",
+  },
 }
