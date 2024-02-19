e639f16707e8562b1e652562453291694df2a38b
zioufang <zioufang@gmail.com>
2024-02-19 12:45:17 +1100

    add more installed treesitter parser

--- lua/zi/plugins/treesitter.lua
+++ lua/zi/plugins/treesitter.lua
@@ -1,58 +1,58 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = function()
-			pcall(require("nvim-treesitter.install").update({ with_sync = true }))
-		end,
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				-- A list of parser names, or "all"
-				ensure_installed = { "python", "rust", "vim", "lua", "javascript", "typescript", "go" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = function()
+      pcall(require("nvim-treesitter.install").update({ with_sync = true }))
+    end,
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        -- A list of parser names, or "all"
+        ensure_installed = { "python", "rust", "lua", "javascript", "typescript", "go", "bash", "dockerfile", "yaml", "json", "toml", "sql" },
 
-				-- Install parsers synchronously (only applied to `ensure_installed`)
-				sync_install = false,
+        -- Install parsers synchronously (only applied to `ensure_installed`)
+        sync_install = false,
 
-				-- Automatically install missing parsers when entering buffer
-				-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-				auto_install = true,
+        -- Automatically install missing parsers when entering buffer
+        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+        auto_install = true,
 
-				highlight = {
-					-- `false` will disable the whole extension
-					enable = true,
+        highlight = {
+          -- `false` will disable the whole extension
+          enable = true,
 
-					-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-					-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-					-- Using this option may slow down your editor, and you may see some duplicate highlights.
-					-- Instead of true it can also be a list of languages
-					additional_vim_regex_highlighting = false,
-				},
-			})
+          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+          -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+          -- Using this option may slow down your editor, and you may see some duplicate highlights.
+          -- Instead of true it can also be a list of languages
+          additional_vim_regex_highlighting = false,
+        },
+      })
 
-			require("nvim-treesitter.configs").setup({
-				textobjects = {
-					move = {
-						enable = true,
-						set_jumps = false, -- whether to set jumps in the jumplist
-						goto_next_start = {
-							["]]"] = "@function.outer",
-						},
-						goto_next_end = {
-							["]["] = "@function.outer",
-						},
-						goto_previous_start = {
-							["[["] = "@function.outer",
-						},
-						goto_previous_end = {
-							["[]"] = "@function.outer",
-						},
-					},
-				},
-			})
-		end,
-	},
-	{
-		"nvim-treesitter/nvim-treesitter-textobjects",
-		event = "VeryLazy",
-		dependencies = "nvim-treesitter",
-	},
+      require("nvim-treesitter.configs").setup({
+        textobjects = {
+          move = {
+            enable = true,
+            set_jumps = false, -- whether to set jumps in the jumplist
+            goto_next_start = {
+              ["]]"] = "@function.outer",
+            },
+            goto_next_end = {
+              ["]["] = "@function.outer",
+            },
+            goto_previous_start = {
+              ["[["] = "@function.outer",
+            },
+            goto_previous_end = {
+              ["[]"] = "@function.outer",
+            },
+          },
+        },
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    event = "VeryLazy",
+    dependencies = "nvim-treesitter",
+  },
 }
