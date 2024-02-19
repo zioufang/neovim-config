9c3be0a375027687a313809285b03a30ffa5e087
zioufang <zioufang@gmail.com>
2024-02-19 11:49:40 +1100

    remove winshift

--- lua/zi/plugins/windows.lua
+++ lua/zi/plugins/windows.lua
@@ -1,48 +1,30 @@
 return {
-	-- tmux
-	{
-		"numToStr/Navigator.nvim",
-		config = true,
-	},
+  -- tmux
+  {
+    "numToStr/Navigator.nvim",
+    config = true,
+  },
 
-	-- maximize with gaps
-	{
-		"anuvyklack/windows.nvim",
-		keys = {
-			{ "<F12>", "<Cmd>WindowsMaximize<Cr>", mode = { "n", "t" } },
-			{ "<F11>", "<Cmd>WindowsMaximizeVertically<Cr>", mode = { "n", "t" } },
-		},
-		dependencies = {
-			"anuvyklack/middleclass",
-			"anuvyklack/animation.nvim",
-		},
-		config = function()
-			vim.o.winwidth = 5
-			vim.o.winminwidth = 5
-			vim.o.equalalways = false
-			require("windows").setup({
-				autowidth = {
-					enable = false,
-				},
-			})
-		end,
-	},
-
-	-- swapping windows
-	{
-		"sindrets/winshift.nvim",
-		enabled = false,
-		config = function()
-			require("winshift").setup({
-				window_picker = function()
-					return require("winshift.lib").pick_window({
-						picker_chars = "SDFGHJKL1234567890",
-					})
-				end,
-			})
-			-- vim.keymap.set("n", "<C-W>M", "<Cmd>WinShift<Cr>")
-			-- need to set cmdheight to non-zero, so the selection shows up. set it back to one afterwards (:set ch=0)
-			vim.keymap.set("n", "<C-W>m", "<Cmd>set cmdheight=1<Cr><Cmd>WinShift swap<Cr>")
-		end,
-	},
+  -- maximize with gaps
+  {
+    "anuvyklack/windows.nvim",
+    keys = {
+      { "<F12>", "<Cmd>WindowsMaximize<Cr>",           mode = { "n", "t" } },
+      { "<F11>", "<Cmd>WindowsMaximizeVertically<Cr>", mode = { "n", "t" } },
+    },
+    dependencies = {
+      "anuvyklack/middleclass",
+      "anuvyklack/animation.nvim",
+    },
+    config = function()
+      vim.o.winwidth = 5
+      vim.o.winminwidth = 5
+      vim.o.equalalways = false
+      require("windows").setup({
+        autowidth = {
+          enable = false,
+        },
+      })
+    end,
+  },
 }
