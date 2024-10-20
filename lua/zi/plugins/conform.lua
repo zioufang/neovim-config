58c5123a33290e4848b44cf578a103e4fde49236
zioufang <zioufang@gmail.com>
2024-10-21 10:50:54 +1100

    update

--- lua/zi/plugins/conform.lua
+++ lua/zi/plugins/conform.lua
@@ -4,13 +4,15 @@ return {
     formatters_by_ft = {
       -- Conform will run multiple formatters sequentially
       python = { "isort", "black" },
+      -- ruff remove unused import, which can be annoying at the time
+      -- python = { "ruff" },
       go = { "goimports", "gofmt" },
       markdown = { "prettierd" },
       sh = { "shfmt" },
       -- Use a sub-list to run only the first available formatter
-      javascript = { { "prettierd", "prettier" } },
-      typescript = { { "prettierd", "prettier" } },
-      typescriptreact = { { "prettierd", "prettier" } },
+      javascript = { "prettierd", "prettier", stop_after_first = true },
+      typescript = { "prettierd", "prettier", stop_after_first = true },
+      typescriptreact = { "prettierd", "prettier", stop_after_first = true },
     },
     format_on_save = {
       timeout_ms = 500,
