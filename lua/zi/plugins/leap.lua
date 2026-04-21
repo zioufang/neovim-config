fe61fc649cfee5726086d0761e1b0f9d6baf6470
zioufang <zioufang@gmail.com>
2026-04-21 13:57:56 +1000

    fix

--- lua/zi/plugins/leap.lua
+++ lua/zi/plugins/leap.lua
@@ -5,7 +5,7 @@ return {
       require("leap").opts.safe_labels = {}
       require("leap").opts.labels =
       { "f", "d", "s", "g", "j", "k", "l", "h", "r", "t", "v", "b", "y", "u", "n", "a" }
-      require("leap").opts.case_sensitive = true
+      require('leap').opts.vim_opts['go.ignorecase'] = false
 
       require('leap.user').set_repeat_keys('<enter>', '<backspace>', {
         -- When true, repeat in the direction of the last motion
