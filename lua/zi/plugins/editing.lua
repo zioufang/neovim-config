94b4eaf51dc69e468e79088942db84bec6d2748b
zioufang <zioufang@gmail.com>
2026-04-27 12:05:42 +1000

    mini comment

--- lua/zi/plugins/editing.lua
+++ lua/zi/plugins/editing.lua
@@ -1,18 +1,4 @@
 return {
-  -- gc for comment
-  {
-    "numToStr/Comment.nvim",
-    event = "VeryLazy",
-    config = function()
-      local ft = require('Comment.ft')
-      ft.set('hcl', '# %s')
-      require("Comment").setup({
-        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-        ignore = '^$', -- ignore empty lines
-      })
-    end,
-  },
-
   {
     "windwp/nvim-autopairs",
     event = "VeryLazy",
---
+++ lua/zi/plugins/mini.lua
@@ -0,0 +1,3 @@
+return {
+  { 'nvim-mini/mini.comment', version = false },
+}
